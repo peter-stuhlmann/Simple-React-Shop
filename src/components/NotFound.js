747c644a63fe3f61ea62460fdbef05ceb9e1c810
Peter R. Stuhlmann <info@peter-stuhlmann.de>
2019-04-24 11:58:48 +0200

    Create component for error 404

---
+++ src/components/NotFound.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react';
+
+export default class NotFound extends Component {
+    render() {
+      return (
+        <React.Fragment>
+            <h1>Error 404</h1>
+            <p>Sorry, this page does not exist (anymore).</p>
+        </React.Fragment>
+    );
+  }
+}
