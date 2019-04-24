494550cbb43b5c7d6114cf5e2d3b8f568047cb65
Peter R. Stuhlmann <info@peter-stuhlmann.de>
2019-04-24 15:16:56 +0200

    Add some placeholder products

--- src/components/Products.js
+++ src/components/Products.js
@@ -5,7 +5,36 @@ export default class Products extends Component {
       return (
         <React.Fragment>
           <h1>Products</h1>
-          <p>Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet. Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet.</p>
+          <div className="products">
+            <div className="product">
+              <span className="product-title">Product #1</span>
+              <p className="product-description">Lorem ipsum dolor sit amet, consectetuer adipiscing elit.</p>
+              <span className="product-price">€ 4.99</span>
+              <input min="0" type="number"></input>
+              <button type="button">Add to cart</button>
+            </div>
+            <div className="product">
+              <span className="product-title">Product #2</span>
+              <p className="product-description">Lorem ipsum dolor sit amet, consectetuer adipiscing elit.</p>
+              <span className="product-price">€ 12.99</span>
+              <input min="0" type="number"></input>
+              <button type="button">Add to cart</button>
+            </div>
+            <div className="product">
+              <span className="product-title">Product #3</span>
+              <p className="product-description">Lorem ipsum dolor sit amet, consectetuer adipiscing elit.</p>
+              <span className="product-price">€ 42.95</span>
+              <input min="0" type="number"></input>
+              <button type="button">Add to cart</button>
+            </div>
+            <div className="product">
+              <span className="product-title">Product #4</span>
+              <p className="product-description">Lorem ipsum dolor sit amet, consectetuer adipiscing elit.</p>
+              <span className="product-price">€ 2.99</span>
+              <input min="0" type="number"></input>
+              <button type="button">Add to cart</button>
+            </div>
+          </div>
         </React.Fragment>
     );
   }
