18923e3f87366604a9227a12ee0ae19697ec8671
Peter R. Stuhlmann <info@peter-stuhlmann.de>
2019-04-24 12:52:07 +0200

    Create route components and import them in index.js

---
+++ src/components/LegalNotice.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react';
+
+export default class LegalNotice extends Component {
+    render() {
+      return (
+        <React.Fragment>
+          <h1>Impressum (Legal Notice)</h1>
+          <p>Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet. Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet.</p>
+        </React.Fragment>
+    );
+  }
+}
--- src/index.js
+++ src/index.js
@@ -1,13 +1,25 @@
 import React, { Component } from 'react';
 import ReactDOM from 'react-dom';
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 import './assets/scss/style.scss';
+import Products from './components/Products';
+import ShoppingCart from './components/ShoppingCart';
+import LegalNotice from './components/LegalNotice';
+import PrivacyPolicy from './components/PrivacyPolicy';
+import NotFound from './components/NotFound';
 
 export default class App extends Component {
-    render() {
-      return (
-        <React.Fragment>
-
-        </React.Fragment>
+  render() {
+    return (
+      <Router>
+        <Switch>
+          <Route exact path="/" component={ Products } />
+          <Route path="/cart" component={ ShoppingCart } />
+          <Route path="/legalnotice" component={ LegalNotice } />
+          <Route path="/privacypolicy" component={ PrivacyPolicy } />
+          <Route component={ NotFound } />
+        </Switch>
+      </Router>
     );
   }
 }
