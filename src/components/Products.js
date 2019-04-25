d4ddb2d97462e8be7f22b64845d0767307992152
Peter R. Stuhlmann <info@peter-stuhlmann.de>
2019-04-25 12:05:57 +0200

    Fetch the data from ProductCatalog

--- src/components/Products.js
+++ src/components/Products.js
@@ -1,41 +1,31 @@
 import React, { Component } from 'react';
+import ProductList from './ProductList';
 
 export default class Products extends Component {
-    render() {
-      return (
-        <React.Fragment>
-          <h1>Products</h1>
-          <div className="products">
-            <div className="product">
-              <span className="product-title">Product #1</span>
-              <p className="product-description">Lorem ipsum dolor sit amet, consectetuer adipiscing elit.</p>
-              <span className="product-price">€ 4.99</span>
-              <input min="0" type="number"></input>
-              <button type="button">Add to cart</button>
-            </div>
-            <div className="product">
-              <span className="product-title">Product #2</span>
-              <p className="product-description">Lorem ipsum dolor sit amet, consectetuer adipiscing elit.</p>
-              <span className="product-price">€ 12.99</span>
-              <input min="0" type="number"></input>
-              <button type="button">Add to cart</button>
-            </div>
-            <div className="product">
-              <span className="product-title">Product #3</span>
-              <p className="product-description">Lorem ipsum dolor sit amet, consectetuer adipiscing elit.</p>
-              <span className="product-price">€ 42.95</span>
-              <input min="0" type="number"></input>
-              <button type="button">Add to cart</button>
-            </div>
-            <div className="product">
-              <span className="product-title">Product #4</span>
-              <p className="product-description">Lorem ipsum dolor sit amet, consectetuer adipiscing elit.</p>
-              <span className="product-price">€ 2.99</span>
-              <input min="0" type="number"></input>
-              <button type="button">Add to cart</button>
-            </div>
-          </div>
-        </React.Fragment>
+
+  constructor(props) {
+    super(props)
+    this.state = {
+      items: []
+    }
+  }
+
+  componentDidMount() {
+    fetch('ProductCatalog.json')
+      .then(response => response.json())
+      .then(product => this.setState(state => state.items = product))
+  }
+ 
+  handleClick = (event) => {
+    console.log('Click')
+  }
+
+  render() {
+    return (
+      <React.Fragment>
+        <h1>Products</h1>
+        <ProductList items={this.state.items} handleClick={this.handleClick} />
+      </React.Fragment>
     );
   }
 }
