370254b9dc3652738e00b142bb38b73a5a21380b
Peter R. Stuhlmann <info@peter-stuhlmann.de>
2019-04-26 13:42:24 +0200

    Add product rating
    Depending on the data in the json file, rating stars (FontAwesome) will be displayed

--- src/components/ProductList.js
+++ src/components/ProductList.js
@@ -20,6 +20,32 @@ export default class ProductList extends Component {
     return lowAvailability
   }
 
+  productRating = (rating) => {
+    let productRating = ""
+    if (rating == 5) {
+      productRating = <React.Fragment><div className="product-rating" title={rating}><span className="fas fa-star"></span><span className="fas fa-star"></span><span className="fas fa-star"></span><span className="fas fa-star"></span><span className="fas fa-star"></span></div></React.Fragment>
+    } else if (rating >= 4.5 && rating < 5) {
+      productRating = <React.Fragment><div className="product-rating" title={rating}><span className="fas fa-star"></span><span className="fas fa-star"></span><span className="fas fa-star"></span><span className="fas fa-star"></span><span className="fas fa-star-half-alt"></span></div></React.Fragment>
+    } else if (rating >= 4 && rating < 4.5) {
+      productRating = <React.Fragment><div className="product-rating" title={rating}><span className="fas fa-star"></span><span className="fas fa-star"></span><span className="fas fa-star"></span><span className="fas fa-star"></span><span className="far fa-star"></span></div></React.Fragment>
+    } else if (rating >= 3.5 && rating < 4) {
+      productRating = <React.Fragment><div className="product-rating" title={rating}><span className="fas fa-star"></span><span className="fas fa-star"></span><span className="fas fa-star"></span><span className="fas fa-star-half-alt"></span><span className="far fa-star"></span></div></React.Fragment>
+    } else if (rating >= 3 && rating < 3.5) {
+      productRating = <React.Fragment><div className="product-rating" title={rating}><span className="fas fa-star"></span><span className="fas fa-star"></span><span className="fas fa-star"></span><span className="far fa-star"></span><span className="far fa-star"></span></div></React.Fragment>
+    } else if (rating >= 2.5 && rating < 3) {
+      productRating = <React.Fragment><div className="product-rating" title={rating}><span className="fas fa-star"></span><span className="fas fa-star"></span><span className="fas fa-star-half-alt"></span><span className="far fa-star"></span><span className="far fa-star"></span></div></React.Fragment>
+    } else if (rating >= 2 && rating < 2.5) {
+      productRating = <React.Fragment><div className="product-rating" title={rating}><span className="fas fa-star"></span><span className="fas fa-star"></span><span className="far fa-star"></span><span className="far fa-star"></span><span className="far fa-star"></span></div></React.Fragment>
+    } else if (rating >= 1.5 && rating < 2) {
+      productRating = <React.Fragment><div className="product-rating" title={rating}><span className="fas fa-star"></span><span className="fas fa-star-half-alt"></span><span className="far fa-star"></span><span className="far fa-star"></span><span className="far fa-star"></span></div></React.Fragment>
+    } else if (rating >= 1 && rating < 1.5) {
+      productRating = <React.Fragment><div className="product-rating" title={rating}><span className="fas fa-star"></span><span className="far fa-star"></span><span className="far fa-star"></span><span className="far fa-star"></span><span className="far fa-star"></span></div></React.Fragment>
+    } else {
+      productRating = <em>No ratings available.</em>
+    }
+    return productRating
+  }
+
   render() {
     return (
       <ul className="product-list">
@@ -28,6 +54,7 @@ export default class ProductList extends Component {
             <li key={item.id} className="product-item">
               <span className="product-title">{item.title}</span>
               <div className="product-preview"><img src={item.preview} /></div>
+              <span className="product-rating">{this.productRating(item.rating)}</span>
               <p className="product-description">{item.description}</p>
               <span className="product-price">€ {item.price}</span><br />
               {this.productAvailability(item.stock)}
