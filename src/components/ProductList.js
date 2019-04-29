511ae55b6d1d9a017532c81ac4a4160796eeebd5
Peter R. Stuhlmann <info@peter-stuhlmann.de>
2019-04-29 23:15:14 +0200

    Add a label for 'select' and remove unnecessary classes

--- src/components/ProductList.js
+++ src/components/ProductList.js
@@ -25,23 +25,23 @@ export default class ProductList extends Component {
   productRating = (rating) => {
     let productRating = ""
     if (rating == 5) {
-      productRating = <React.Fragment><div className="product-rating" title={rating}><span className="fas fa-star"></span><span className="fas fa-star"></span><span className="fas fa-star"></span><span className="fas fa-star"></span><span className="fas fa-star"></span></div></React.Fragment>
+      productRating = <React.Fragment><div title={rating}><span className="fas fa-star"></span><span className="fas fa-star"></span><span className="fas fa-star"></span><span className="fas fa-star"></span><span className="fas fa-star"></span></div></React.Fragment>
     } else if (rating >= 4.5 && rating < 5) {
-      productRating = <React.Fragment><div className="product-rating" title={rating}><span className="fas fa-star"></span><span className="fas fa-star"></span><span className="fas fa-star"></span><span className="fas fa-star"></span><span className="fas fa-star-half-alt"></span></div></React.Fragment>
+      productRating = <React.Fragment><div title={rating}><span className="fas fa-star"></span><span className="fas fa-star"></span><span className="fas fa-star"></span><span className="fas fa-star"></span><span className="fas fa-star-half-alt"></span></div></React.Fragment>
     } else if (rating >= 4 && rating < 4.5) {
-      productRating = <React.Fragment><div className="product-rating" title={rating}><span className="fas fa-star"></span><span className="fas fa-star"></span><span className="fas fa-star"></span><span className="fas fa-star"></span><span className="far fa-star"></span></div></React.Fragment>
+      productRating = <React.Fragment><div title={rating}><span className="fas fa-star"></span><span className="fas fa-star"></span><span className="fas fa-star"></span><span className="fas fa-star"></span><span className="far fa-star"></span></div></React.Fragment>
     } else if (rating >= 3.5 && rating < 4) {
-      productRating = <React.Fragment><div className="product-rating" title={rating}><span className="fas fa-star"></span><span className="fas fa-star"></span><span className="fas fa-star"></span><span className="fas fa-star-half-alt"></span><span className="far fa-star"></span></div></React.Fragment>
+      productRating = <React.Fragment><div title={rating}><span className="fas fa-star"></span><span className="fas fa-star"></span><span className="fas fa-star"></span><span className="fas fa-star-half-alt"></span><span className="far fa-star"></span></div></React.Fragment>
     } else if (rating >= 3 && rating < 3.5) {
-      productRating = <React.Fragment><div className="product-rating" title={rating}><span className="fas fa-star"></span><span className="fas fa-star"></span><span className="fas fa-star"></span><span className="far fa-star"></span><span className="far fa-star"></span></div></React.Fragment>
+      productRating = <React.Fragment><div title={rating}><span className="fas fa-star"></span><span className="fas fa-star"></span><span className="fas fa-star"></span><span className="far fa-star"></span><span className="far fa-star"></span></div></React.Fragment>
     } else if (rating >= 2.5 && rating < 3) {
-      productRating = <React.Fragment><div className="product-rating" title={rating}><span className="fas fa-star"></span><span className="fas fa-star"></span><span className="fas fa-star-half-alt"></span><span className="far fa-star"></span><span className="far fa-star"></span></div></React.Fragment>
+      productRating = <React.Fragment><div title={rating}><span className="fas fa-star"></span><span className="fas fa-star"></span><span className="fas fa-star-half-alt"></span><span className="far fa-star"></span><span className="far fa-star"></span></div></React.Fragment>
     } else if (rating >= 2 && rating < 2.5) {
-      productRating = <React.Fragment><div className="product-rating" title={rating}><span className="fas fa-star"></span><span className="fas fa-star"></span><span className="far fa-star"></span><span className="far fa-star"></span><span className="far fa-star"></span></div></React.Fragment>
+      productRating = <React.Fragment><div title={rating}><span className="fas fa-star"></span><span className="fas fa-star"></span><span className="far fa-star"></span><span className="far fa-star"></span><span className="far fa-star"></span></div></React.Fragment>
     } else if (rating >= 1.5 && rating < 2) {
-      productRating = <React.Fragment><div className="product-rating" title={rating}><span className="fas fa-star"></span><span className="fas fa-star-half-alt"></span><span className="far fa-star"></span><span className="far fa-star"></span><span className="far fa-star"></span></div></React.Fragment>
+      productRating = <React.Fragment><div title={rating}><span className="fas fa-star"></span><span className="fas fa-star-half-alt"></span><span className="far fa-star"></span><span className="far fa-star"></span><span className="far fa-star"></span></div></React.Fragment>
     } else if (rating >= 1 && rating < 1.5) {
-      productRating = <React.Fragment><div className="product-rating" title={rating}><span className="fas fa-star"></span><span className="far fa-star"></span><span className="far fa-star"></span><span className="far fa-star"></span><span className="far fa-star"></span></div></React.Fragment>
+      productRating = <React.Fragment><div title={rating}><span className="fas fa-star"></span><span className="far fa-star"></span><span className="far fa-star"></span><span className="far fa-star"></span><span className="far fa-star"></span></div></React.Fragment>
     } else {
       productRating = <em>No ratings available.</em>
     }
@@ -57,9 +57,11 @@ export default class ProductList extends Component {
         <p className="product-description">{this.props.items.description}</p>
         <span className="product-price">€ {this.props.items.price}</span><br />
         {this.productAvailability(this.props.items.stock)}
-        <select ref={this.inputAmount} defaultValue="0" onChange={()=> console.log(this.select)}>
-          {this.selectProductAmount(this.props.items.stock)}
-        </select> 
+        <label for="qty">Qty:&nbsp;
+          <select name="qty" ref={this.inputAmount} defaultValue="0" onChange={()=> console.log(this.select)}>
+            {this.selectProductAmount(this.props.items.stock)}
+          </select>
+        </label>
         <button type="button" className="submit" aria-label="Submit" onClick={() => { this.props.handleClick( this.props.items.id, Number(this.inputAmount.current.value)); }}><span className="fas fa-cart-plus">&nbsp;</span>Add to cart</button>
       </li >
     )
